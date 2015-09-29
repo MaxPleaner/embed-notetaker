20b6c7f949c92ddeeba4eadb7ee3084643c18b16
maxpleaner <maxpleaner@gmail.com>
2015-09-29 13:19:03 -0700

    some css

--- lib/embed.rb
+++ lib/embed.rb
@@ -12,7 +12,7 @@ class Embed
 			target_name = title.split.join("-")
 			toggling_section = <<-HTML
 				<button class="toggling" target="#{target_name}">#{title}</button>
-				<section id="#{target_name}">#{content}</section>
+				<section id="#{target_name}" class="content" >#{content}</section>
 			HTML
 			toggling_section.html_safe
 		end
