1124fe512fe2777a9938e3fb1d18838d348aa908
maxpleaner <maxpleaner@gmail.com>
2015-09-30 12:51:27 -0700

    working /event/funcheap endpoint

--- lib/funcheap.rb
+++ lib/funcheap.rb
@@ -1,11 +1,28 @@
-# class Funcheap
-# 	attr_reader :events_text
-# 	def initialize
-# 		@url = "http://sf.funcheap.com/events/"
-# 		@events_text = `curl #{@url} | w3m -dump -T text/html`
-# 	end
-# 	def events
-# 		# dates = @event_text.scan
-# 		[@events_text]
-# 	end
-# end
+class Funcheap
+	attr_reader :events_text
+	def initialize
+		@url = "http://sf.funcheap.com/events/"
+		@events_text = `curl #{@url} | w3m -dump -T text/html`
+	end
+	def events(params={})
+			dates = @events_text
+				.split
+				.each_cons(4)
+				.map { |a| a.join(' ') }
+				.select { |s| Date.strptime(s, "%A, %B %d, %Y") rescue nil }
+			dates_regex = /(?=#{ dates.join("|") })/i
+			events = @events_text.split(dates_regex).map do |str|
+				if dates.include?(str) || str.match(/Want to narrow/)
+					""
+				else
+					str.split("»Last »")[0]
+				end
+			end.reject(&:empty?)
+			dates.reduce([]) do |results, date|
+				results << {
+					name: date,
+					content: "<pre>#{events[results.length]}</pre>".html_safe
+				}; results
+			end
+	end
+end
