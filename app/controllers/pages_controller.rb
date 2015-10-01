5dd8b98570d09afe154f9c24411f86d82a889206
maxpleaner <maxpleaner@gmail.com>
2015-09-30 18:47:47 -0700

    get indybay working except descriptions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+
   def index
   	@notes = Note.all
   	@note = Note.new
--- lib/indybay.rb
+++ lib/indybay.rb
@@ -1,7 +1,47 @@
 class Indybay
+	attr_reader :xml_doc
 	def initialize
+		url = "http://www.indybay.org/syn/generate_rss.php?include_posts=0&include_attachments=0&include_events=0&include_events=1&page_id=36"
+		@xml_doc = Nokogiri.parse(open(url))
 	end
-	def events
-		[Event.new("unimplemented")]
+	def events(params={})
+		groups = xml_doc.css("item").reduce({}) do |results, item|
+			date = DateTime.strptime(item.css("date")[0].text, "%Y-%m-%dT%H:%M")
+			date_key = date.strftime("%A, %b %d")
+			time_of_day = nil #date.strftime("%l:%M %P")
+			
+			img = item.try(:css, "enclosure")
+			img = img.to_a.empty? ? nil : img.try(:attr, "url").try(:first).try(:text)
+
+			title = item.try(:css, "title")
+			title = title.to_a.empty? ? nil : title.try(:first).try(:text)
+			
+			creator = item.try(:css, "creator")
+			creator = creator.to_a.empty? ? nil : creator.try(:first).try(:text)
+			
+			link = item.try(:css, "link")
+			link = link.to_a.empty? ? nil : link.try(:first).try(:text)
+			
+			desc = item.try(:css, "description")
+			desc = desc.to_a.empty? ? nil : desc.try(:first).try(:text)
+
+			results[date_key] ||= []
+			results[date_key] << <<-TXT
+				#{("<b>" + title + "</b><br>") if title}
+				#{("<a href='" + link + "'>link</a><br>") if link}
+				#{("<p>" + time_of_day + "</p>") if time_of_day}
+				#{("<p>" + creator + "</p>") if creator}
+				#{("<i>" + desc + "</i><br>") if desc}
+				#{("<img src='" + img + "'>") if img}
+			TXT
+			results
+		end
+		groups
+			.reduce([]) do |arr, (date, content)|
+				arr << {
+					name: date,
+					content: content.join("<hr>").html_safe
+				}; arr
+			end
 	end
 end
